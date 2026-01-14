efb4aa85de4413735fd8253de4b45cd9d759d72b
José Lucas Sousa Ferreira <j.lucassousafer@gmail.com>
2026-01-14 11:24:13 -0300

    feat(domain): add LogAction enum for audit logging

---
+++ Domain/Enums/LogAction.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Enums
+{
+    public enum LogAction
+    {
+        PRODUCT_CREATED = 1,
+        PRODUCT_UPDATED = 2,
+        PRODUCT_DELETED = 3,
+        CATEGORY_CREATED = 4,
+        CATEGORY_UPDATED = 5,
+        CATEGORY_DELETED = 6
+    }
+}
